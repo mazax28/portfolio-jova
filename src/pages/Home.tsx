e109cde5acf41b02222b76f9ba00968ca8e0a0eb
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-21 18:25:43 -0400

    feat: add id attribute to Home section and improve text styling for better readability

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 
 function Home() {
   return (
-    <div className="hero min-h-screen">
+    <div id="home" className="hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row justify-between w-full">
         
         {/* Columna 1 - Título alineado a la izquierda y abajo */}
@@ -15,7 +15,7 @@ function Home() {
         <div className="flex-1 flex items-start justify-end">
           <div className="card max-w-sm border border-[#fafafa] rounded-lg shadow-2xl p-6">
             <h2 className="text-2xl font-bold mb-2">Hola, soy Jorge Vazquez</h2>
-            <p className="text-[14px] font-semibold ">
+            <p className="text-[14px] text-gray-300 font-semibold ">
               Soy un estudiante apasionado por el diseño arquitectónico, con enfoque en la innovación y sostenibilidad.
             </p>
           </div>
