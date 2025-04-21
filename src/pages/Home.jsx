fd89a35e7f56d397a20bc1e799269277f123a1a2
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-21 13:11:42 -0400

    feat: create Home component with responsive layout and introductory content

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row justify-between w-full">
+        
+        {/* Columna 1 - Título alineado a la izquierda y abajo */}
+        <div className="flex-1 flex items-end justify-start">
+          <h1 className="text-[60px] md:text-[80px] lg:text-[112px] font-semibold leading-tight">
+            Estudiante <br />de <br />Arquitectura
+          </h1>
+        </div>
+
+        {/* Columna 2 - Card alineado arriba */}
+        <div className="flex-1 flex items-start justify-end">
+          <div className="card max-w-sm border border-[#fafafa] rounded-lg shadow-2xl p-6">
+            <h2 className="text-2xl font-bold mb-2">Hola, soy Jorge Vazquez</h2>
+            <p className="text-[14px] font-semibold ">
+              Soy un estudiante apasionado por el diseño arquitectónico, con enfoque en la innovación y sostenibilidad.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Home;
