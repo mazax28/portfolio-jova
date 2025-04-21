d928dec53df3d07dbaf410ef399f811aba5659a0
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-21 13:10:18 -0400

    feat: add Footer component with social media links

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Footer() {
+  return (
+<footer className="footer footer-horizontal footer-center  text-base-content rounded p-10">
+  
+  <nav>
+    <div className="grid grid-flow-col gap-4">
+      <a className='btn btn-outline hover:bg-[#09090b]'>
+        <i class="ri-twitter-x-line"></i>
+      </a>
+      <a className='btn btn-outline hover:bg-[#09090b]'>
+      <i class="ri-linkedin-line"></i>
+      </a>
+      <a className='btn btn-outline hover:bg-[#09090b]'>
+      <i class="ri-youtube-line"></i>
+      </a>
+    </div>
+  </nav>
+</footer>
+  )
+}
+
+export default Footer
